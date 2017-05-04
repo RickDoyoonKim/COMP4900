1f79c797597d915f69101e89c0ebfb5be6202f33
Edward You <taehun0623@daum.net>
2017-05-04 11:55:50 -0700

    Really?

--- ImpactWebsite/Controllers/AccountController.cs
+++ ImpactWebsite/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace ImpactWebsite.Controllers
         private readonly ILogger _logger;
         private readonly string _externalCookieScheme;
 
-        //Edward You Pull Request Test!
+        //Edward You Pull Request Test!!!
         public AccountController(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
