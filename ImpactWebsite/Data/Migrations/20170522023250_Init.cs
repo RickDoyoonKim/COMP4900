70c4b2b0b5cfa58dfd6b305914b58ad517ca7fa6
jakejkim <jakejkim11@gmail.com>
2017-05-21 19:37:47 -0700

    Edit for deployment

--- ImpactWebsite/Data/Migrations/20170522023250_Init.cs
+++ ImpactWebsite/Data/Migrations/20170522023250_Init.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace ImpactWebsite.Migrations
 {
-    public partial class Migration0 : Migration
+    public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
